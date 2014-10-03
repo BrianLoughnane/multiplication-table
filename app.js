a2063e5f2a0d0795cfaef217d7992e5a2c60cdcf
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-03 17:06:46 -0400

    hard coded values - v.1

---
+++ app.js
@@ -0,0 +1,5 @@
+angular.module('myApp', []).controller('MultiplicationController',['$scope', 
+	function($scope) {
+		// $scope.numbers = range(1,10,1);
+		$scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+}]);
