22cf3ded41cefd93fe32406727f6f9cc4de54efb
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-04 19:04:33 -0400

    styling plus new controller added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-angular.module('myApp', []).controller('MultiplicationController', 
-	function($scope, $attrs) {
+angular.module('myApp', [])
+	.controller('DisplayController', function($scope) {
+		// $scope.content = 'hi';
+		$scope.$on('displayData', function(event, data) {
+			$scope.content = data;
+		});
+	})
+	.controller('MultiplicationController', 
+	function($scope, $attrs, $rootScope) {
 		function populateNumbers(x) {
 			var numbers = [];
-			for(var i = 0; i <= x; i++) {
+			for(var i = 0; i < x; i++) {
 				numbers[i] = i + 1;
 			}
 
@@ -18,5 +25,23 @@ angular.module('myApp', []).controller('MultiplicationController',
 		});
 
 		$scope.numberLimit = $attrs.initialNumberLimit || 10;
-		$scope.name = "brian";
-});
+		
+		$scope.activeFactors = [];
+		
+		$scope.setActiveFactors = function(a,b) {
+			$scope.activeFactors[0] = a;
+			$scope.activeFactors[1] = b;
+		};
+
+		$scope.matchesFactor = function(a,b) {
+			return a === $scope.activeFactors[0] || b === $scope.activeFactors[1];
+			// return a === $scope.activeFactors[0] || b === ;
+		};
+
+		$scope.setActiveNumber = function(number) {
+			$rootScope.$broadcast('displayData', number);
+		};
+
+
+
+	});
